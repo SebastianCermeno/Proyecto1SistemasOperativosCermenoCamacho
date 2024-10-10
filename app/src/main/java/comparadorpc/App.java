cf7e2643d80c35d247b28395d11f9f9b0dafcf95
Sebastián <sebastian-cermeno@correo.unimet.edu.ve>
2024-10-10 18:17:16 -0400

    BUG FIXING WARS I: The UI Menace

--- app/src/main/java/comparadorpc/App.java
+++ app/src/main/java/comparadorpc/App.java
@@ -1,12 +1,14 @@
 package comparadorpc;
 
 import comparadorpc.MainContainer;
+import ui.HomeScreen;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
         MainContainer core = new MainContainer();
         LogicMaster puppetMaster = new LogicMaster();
+        HomeScreen screen1 = new HomeScreen(puppetMaster);
         puppetMaster.executeSimulation();
         core.begin();
     }
